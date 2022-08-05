3fe0b6bc68980847c261d5a6f441ae0790eeb00d
Maksym Morenko <morenko.maxim@gmail.com>
2022-08-05 17:34:33 +0300

    добавил два инпута серч и селект

---
+++ src/components/Controls.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+import { Search } from './Search';
+import { CustomSelect } from './CustomSelect';
+
+const options = [
+    { value: '0-18', label: '0-18' },
+    { value: '18-25', label: '18-25' },
+    { value: '25-35', label: '25-35' },
+    { value: '35-45', label: '35-45' },
+    { value: '45-60', label: '45-60' },
+    { value: '60-75', label: '60-75' },
+    { value: 'all', label: 'all' },
+];
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    @media (min-width: 767px) {
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+`;
+
+export const Controls = ({ onSearch }) => {
+    const [search, setSearch] = useState('');
+    const [age, setAge] = useState('');
+
+    //   useEffect(() => {
+    //     const ageValue = age?.value || '';
+    //     onSearch(search, ageValue);
+
+    //     // eslint-disable-next-line
+    //   }, [search, age]);
+
+    return (
+        <Wrapper>
+            {/* компонент Search ожидаем пропсы search и setSearch, передаем их ему*/}
+        <Search search={search} setSearch={setSearch} />
+        <CustomSelect
+        // передаем пропсы
+            options={options}
+            placeholder="Filter by age"
+            // возможность отменить чтото по крестику
+            isClearable
+            // отменяем возможность поиска
+            isSearchable={false}
+            value={age}
+            onChange={setAge}
+        />
+        </Wrapper>
+    );
+}; 
